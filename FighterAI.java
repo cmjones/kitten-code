14aad3533e30fd4b3207e085bcbf6a6e06b85696
Chris Jones <cmjones015@gmail.com>
2013-01-28 17:42:22 -0500

    Changed fighter ai
    Made fighters congregate around a wait point (center of the map),
    waiting for nearby allies before continuing on.  This makes fighters
    attack in waves rather than conga-lines.  Only fighters have this
    new logic, so fighters should be used when you want this logic.

--- FighterAI.java
+++ FighterAI.java
@@ -2,6 +2,7 @@ package team197;
 
 import battlecode.common.Direction;
 import battlecode.common.MapLocation;
+import battlecode.common.Robot;
 import battlecode.common.RobotController;
 
 import team197.modules.NavModule;
@@ -9,33 +10,62 @@ import team197.modules.FightModule;
 
 
 /** Simple rusher using the fight code.
- * Rushes towards the enemy base.  If enemies
- * are encountered, switches to fighting mode.
+ * First, moves towrds a wait point.  Once enough
+ *  allies are nearby, proceeds towards the kill
+ *  point.  This should make fighters attack in
+ *  waves.
  */
 public class FighterAI extends SoldierAI {
+    private static final int WAIT_DIST = 25,
+                             WAIT_NUM = 10;
+
+    MapLocation wait_point,
+                kill_point;
     int channel_listen;
+    boolean wait;
 
     public FighterAI(RobotController rc, int channel) {
         super(rc);
-        nav.setDestination(rc, rc.senseEnemyHQLocation());
+        setTargetPoints(rc);
+        nav.setDestination(rc, wait_point);
         channel_listen = channel;
+        wait = true;
     }
 
     public FighterAI(RobotController rc, FighterAI oldme) {
     	super(rc, oldme);
-        nav.setDestination(rc, rc.senseEnemyHQLocation());
+        wait_point = oldme.wait_point;
+        kill_point = oldme.kill_point;
+        wait = oldme.wait;
+        nav.setDestination(rc, wait_point);
         channel_listen = oldme.channel_listen;
     }
 
     public FighterAI(RobotController rc, SoldierAI oldme, int channel) {
     	super(rc, oldme);
-        nav.setDestination(rc, rc.senseEnemyHQLocation());
+        setTargetPoints(rc);
+        nav.setDestination(rc, wait_point);
         channel_listen = channel;
+        wait = true;
+    }
+
+    private void setTargetPoints(RobotController rc) {
+        MapLocation tmp;
+
+        tmp = rc.senseHQLocation();
+
+        // Set the kill point to be the enemy.
+        kill_point = rc.senseEnemyHQLocation();
+
+        // Set the wait point to be half-way
+        //  between the hqs.
+        wait_point = new MapLocation((tmp.x+kill_point.x)/2, (tmp.y+kill_point.y)/2);
     }
 
     public AI act(RobotController rc) throws Exception {
         Direction d;
-        MapLocation target;
+        MapLocation target,
+                    cur;
 
 /*        if(channel_listen != 0 && waypoint_heard == null){
         	// Loop until waypoints start appearing
@@ -61,18 +91,29 @@ public class FighterAI extends SoldierAI {
         }*/
 
         // If there are enemies to fight, fight! Otherwise,
-        // continue towards the enemy base
+        // continue towards the current destination
         if(rc.isActive()){
+            // Get our location
+            cur = rc.getLocation();
+
             if((d = fight.fightClosestRobot(rc)) == Direction.OMNI)
-                d = nav.moveFlock(rc, 16);
+                d = nav.moveFlock(rc, wait?4:16);
 
             if(d != Direction.NONE && d != Direction.OMNI) {
                 // If there's a mine, defuse it
-                target = rc.getLocation().add(d);
+                target = cur.add(d);
                 if(rc.senseMine(target) != null && rc.senseMine(target) != rc.getTeam())
                     rc.defuseMine(target);
                 else
                     moveSafe(rc, d);
+            }
+
+            // If we're within range of the wait point and there are
+            //  a number of allies around us, move to the kill point
+            if(wait && cur.distanceSquaredTo(wait_point) <= WAIT_DIST &&
+               rc.senseNearbyGameObjects(Robot.class, wait_point, WAIT_DIST, rc.getTeam()).length >= WAIT_NUM) {
+                wait = false;
+                nav.setDestination(rc, kill_point);
             }
         }
 
